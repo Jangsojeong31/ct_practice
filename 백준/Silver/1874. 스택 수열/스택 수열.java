d1d50527e348002a7c5202ba27dc7adacdad58d1
Jangsojeong31 <sojung301@naver.com>
2025-10-02 13:47:54 +0900

    [Silver II] Title: 스택 수열, Time: 884 ms, Memory: 105928 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+        	arr[i] = sc.nextInt();
+        }
+        
+        Stack<Integer> stack = new Stack<>();
+        int num = 1;
+        boolean result = true;
+        StringBuffer bf = new StringBuffer();
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            if (temp >= num) {
+                while ( temp >= num ) {
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            } else {
+                int n = stack.pop();
+                if ( n > temp ) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                } else {
+                    bf.append("-\n");
+                }
+            }
+        }
+        
+        if (result) System.out.println(bf.toString());
+    }
+}
